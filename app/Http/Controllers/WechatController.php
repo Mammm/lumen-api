4f0ad804d964c7c7f705e9c2aabf04d6943053cc
mapeiren <p@ssw0rd>
2021-04-18 16:20:44 +0800

    [blind-box]csrf问题

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -4,23 +4,33 @@
 namespace App\Http\Controllers;
 
 use App\Services\WechatAccountService;
+use EasyWeChat\OfficialAccount\Application as OfficialAccountService;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Request;
 
 class WechatController extends Controller
 {
-    private $account;
+    private OfficialAccountService $service;
+    private WechatAccountService $account;
 
-    public function __construct(WechatAccountService $account)
+    public function __construct(OfficialAccountService $service,
+                                WechatAccountService $account)
     {
         $this->account = $account;
     }
 
-    public function serve() {
+    public function serve()
+    {
         Log::info("接收到微信消息");
         $officialAccount = app("wechat.official_account");
         $officialAccount->server->push(function ($message) {
             return "欢迎关注公众号";
         });
         return $officialAccount->server->server();
+    }
+
+    public function test(Request $request)
+    {
+        $this->service->oauth->userFromCode($request->input("code"));
     }
 }
